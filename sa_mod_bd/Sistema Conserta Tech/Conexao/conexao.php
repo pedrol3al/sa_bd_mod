4e6bc7f918558fc6e5df5ad917c4be7f34054006
Pedro Gabriel Leal Costa <pedrogabrieleal@gmail.com>
2025-08-12 16:58:32 -0300

    Começo da pagina usuario
    Reestilização e padronização das página de usuario

---
+++ sa_mod_bd/Sistema Conserta Tech/Conexao/conexao.php
@@ -0,0 +1,29 @@
+<?php
+
+$host = 'localhost';
+
+$dbname = 'conserta_tech';
+
+
+$user = 'root';
+
+$pass = '';
+
+
+try {
+
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+} catch (PDOException $e) {
+
+
+    die("Erro de conexão com o banco de dados: " . $e->getMessage());
+}
+
+
+?>
+
