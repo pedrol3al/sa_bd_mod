5f88f0db7ee4c75ca92761b9dfe69c51c4a14047
Pedro Gabriel Leal Costa <pedrogabrieleal@gmail.com>
2025-09-26 09:50:51 -0300

    Update conexao.php

--- sa_mod_bd/Conexao/conexao.php
+++ sa_mod_bd/Conexao/conexao.php
@@ -5,11 +5,18 @@ $username = getenv("MYSQLUSER");
 $password = getenv("MYSQLPASSWORD");
 $port = getenv("MYSQLPORT") ?: "3306";
 
+// Debug: ver se as variáveis estão corretas
+var_dump($host, $port, $dbname, $username, $password);
+
 try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8",
+        $username,
+        $password
+    );
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexão Railway OK!";
 } catch (PDOException $e) {
     die("Erro na conexão: " . $e->getMessage());
 }
 ?>
- 
